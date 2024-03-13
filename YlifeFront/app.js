59bc4da6c63c33e2b9495c93ae1d190ee34a19a0
grégory LOMBARD--PARADIS <gregory332000@hotmail.fr>
2024-03-13 09:52:14 +0100

    push app.js

--- YlifeFront/app.js
+++ YlifeFront/app.js
@@ -18,6 +18,7 @@ const App = () => {
             }
             const data = await response.json();
             console.log(data);
+            navigation.navigate()
         } catch (error) {
             console.error('Error fetching email:', error);
         }
