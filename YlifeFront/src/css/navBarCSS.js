0fbe56a5c4b2b00f877375827b83573b3f4368da
grégory LOMBARD--PARADIS <gregory332000@hotmail.fr>
2024-03-28 19:41:28 +0100

    Positionnement de la navbar vers le bas

--- YlifeFront/src/css/navBarCSS.js
+++ YlifeFront/src/css/navBarCSS.js
@@ -1,5 +1,13 @@
 export default{
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
     navBar: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
